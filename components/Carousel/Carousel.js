1c9ef5ff141c8b417ed1ffb3584acd440597a3cd
Kevin Smith <keveightysev@gmail.com>
2019-02-01 10:30:55 -0600

    Built moveLeft and moveRight methods for carousel. After much frustration, realized I forgot to uncomment the script in the HTML file, but this damn thing is working! Yay!

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,24 +1,55 @@
 class Carousel {
     constructor(carousel) {
         this.carousel = carousel;
-        this.left = carousel.querySelector('.left-button');
-        this.right = carousel.querySelector('.righ-button');
-        this.images = carousel.querySelectorAll('img');
+        this.left = this.carousel.querySelector('.left-button');
+        this.right = this.carousel.querySelector('.right-button');
+        this.images = this.carousel.querySelectorAll('img');
         this.index = 0;
         this.images[this.index].style.display = "block";
+        debugger
         this.left.addEventListener('click', () => this.moveLeft());
         this.right.addEventListener('click', () => this.moveRight());
         this.imgList = Array.from(this.images).map(image => new Slide(image));
     }
 
+    moveLeft() {
+        this.images[this.index].style.display = "none";
+        if (this.index === 0) {
+            this.index = this.images.length - 1;
+        } else {
+            this.index--;
+        }
+        this.images[this.index].style.display = "block";
+        this.imgList.forEach((image, index) => {
+            if (index !== this.index) {
+                image.inactive();
+            }
+        });
+        this.imgList[this.index].active();
+    }
 
+    moveRight() {
+        this.images[this.index].style.display = "none";
+        if (this.index === this.images.length - 1) {
+            this.index = 0;
+        } else {
+            this.index++;
+        }
+        this.images[this.index].style.display = "block";
+        this.imgList.forEach((slide, index) => {
+            if (index !== this.index) {
+                slide.inactive();
+            }
+        });
+        this.imgList[this.index].active();
+    }
 }
 
 class Slide {
     constructor(image) {
         this.image = image;
-        this.index = image.dataset.image;
-        this.slide = document.querySelector(`div[data-image="${this.index}"]`);
+        this.val = this.image.dataset.image;
+        this.slide = document.querySelector(`div[data-image="${this.val}"]`);
     }
 
     active() {
@@ -30,8 +61,8 @@ class Slide {
     }
 }
 
-let carousel = document.querySelectorAll('.carousel');
-carousel.forEach(carousel => new Carousel(carousel));
+let carouselList = document.querySelectorAll('.carousel');
+carouselList.forEach(carousel => new Carousel(carousel));
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
